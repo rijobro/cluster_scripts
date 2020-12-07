a863170722539698489de3fd79dfc49596a6be54
rijobro <33289025+rijobro@users.noreply.github.com>
2020-12-07 10:57:50 +0000

    finished

--- create_runai_monai_im.sh
+++ create_runai_monai_im.sh
@@ -7,15 +7,14 @@ base_image=projectmonai/monai:latest
 im_name=rb-monai
 docker_uname=rijobro
 
-# PW hash created with: openssl passwd -1
-ssh_pw='$1$rI/6m9Sa$3MAazPOmNTJm2IUnnCHOl0'
+# user pw hash created with: openssl passwd -1
+user_pw='$1$rI/6m9Sa$3MAazPOmNTJm2IUnnCHOl0'
 
-
-password=$1
-if [ -z "$password" ]; then
-	read -s -p "Enter root password: " password
-fi
-echo
+#password=$1
+#if [ -z "$password" ]; then
+#	read -s -p "Enter root password: " password
+#fi
+#echo
 
 # This file is used to loop over all groups that the local user is part of,
 # create those groups in the container and add the user to those groups.
@@ -55,6 +54,7 @@ RUN rm Miniconda3-latest-Linux-x86_64.sh
 # Install requirements
 RUN python -m pip install -r https://raw.githubusercontent.com/Project-MONAI/MONAI/master/requirements.txt
 RUN python -m pip install -r https://raw.githubusercontent.com/Project-MONAI/MONAI/master/requirements-dev.txt
+RUN python -m pip install -r https://raw.githubusercontent.com/Project-MONAI/MONAI/master/docs/requirements.txt
 
 # Add user and add to same groups as local
 ARG GROUPS
@@ -67,10 +67,10 @@ RUN adduser --ingroup \${UNAME} --system --shell /bin/bash --uid \${USER_ID} \${
 COPY create_user_groups.sh .
 RUN cat create_user_groups.sh
 RUN bash ./create_user_groups.sh "\$GROUPS" "\$GIDS" \${UNAME}
-#RUN printf "\${UNAME}:%s" "$ssh_pw" | chpasswd -e
-RUN echo "\${UNAME}:$password" | chpasswd
-RUN echo "root:$password" | chpasswd
-RUN echo "+:\${UNAME}:ALL" >> /etc/security/access.conf
+#RUN printf "\${UNAME}:%s" "$user_pw" | chpasswd -e
+#RUN echo "\${UNAME}:$password" | chpasswd
+#RUN echo "root:$password" | chpasswd
+#RUN echo "+:\${UNAME}:ALL" >> /etc/security/access.conf
 
 RUN touch /var/run/motd.new
 
@@ -79,13 +79,12 @@ WORKDIR /home/\${UNAME}
 USER \${UNAME}
 
 # Colourful bash
-RUN echo "PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '" >> ~/.b$
+RUN echo "PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '" >> ~/.bashrc
 
 # Set paths
 ENV PYTHONPATH "~/MONAI:~/ptproto"
 ENV MONAI_DATA_DIRECTORY "~/data/MONAI"
-ENV PATH "/opt/conda/bin:$PATH"
-RUN echo "source ~/bash_profile/rich_bashrc.sh" >> ~/.bashrc
+ENV PATH "/opt/conda/bin:/home/\${UNAME}/.local/bin:$PATH"
 
 # Set up SSHD to be run as non-sudo user
 RUN mkdir -p /home/\${UNAME}/.ssh
@@ -103,6 +102,11 @@ RUN echo "PidFile /home/\${UNAME}/.ssh/sshd.pid" >> /home/\${UNAME}/.ssh/sshd_co
 RUN echo "PrintMotd no" >> /home/\${UNAME}/.ssh/sshd_config
 COPY id_rsa.pub .
 RUN cat ./id_rsa.pub >> ~/.ssh/authorized_keys
+
+# Set up jupyter notebook
+RUN python -m pip install jupyterthemes ipywidgets
+RUN jt -t oceans16 -T -N # Blue theme
+RUN jt -t monokai -f fira -fs 13 -nf ptsans -nfs 11 -N -kl -cursw 5 -cursc r -cellw 95% -T # Green theme
 
 EXPOSE 2222
 
--- monaistartup.sh
+++ monaistartup.sh
@@ -5,121 +5,76 @@ set -x # print command before doing it
 
 
 # Options
-upgrade_python=false
 ssh_server=true
 pulseaudio=false
-compilemonai=false
+compilemonai=true
 runvnc=false
 virtualbox=false
 
 
-
 # Set password hashes. Create with:
-#   bash: openssl passwd -1
 #   jupy: python -c "from notebook.auth import passwd; print(passwd())"
-#ssh_pw='$1$rI/6m9Sa$3MAazPOmNTJm2IUnnCHOl0'
-#jupy_pw='argon2:$argon2id$v=19$m=10240,t=10,p=8$k9uoAnn3KFfJWO3SNMvYmQ$r8E9SnfzkkM4+SiQpIliJw'
-#vnc_pw='monai1'
-
-# Remove default monai folder and mount our own
-#rm -rf /opt/monai/*
-#cd /root
-
-# Colourful bash prompt
-#echo "PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '" >> ~/.bashrc
-
-# Set paths
-#export PYTHONPATH="/root/MONAI:/root/ptproto"
-#export MONAI_DATA_DIRECTORY="/root/data/MONAI"
-#export PATH="/opt/conda/bin:$PATH"
-#printf "export PYTHONPATH=%s\n" "$PYTHONPATH" >> ~/.bashrc
-#printf "export MONA_DATA_DIRECTORY=%s\n" "$MONAI_DATA_DIRECTORY" >> ~/.bashrc
-#printf "export PATH=%s\n" "$PATH" >> ~/.bashrc
+jupy_pw='argon2:$argon2id$v=19$m=10240,t=10,p=8$k9uoAnn3KFfJWO3SNMvYmQ$r8E9SnfzkkM4+SiQpIliJw'
+vnc_pw='monai1'
 
 # Mount our bash script
-#echo "source ~/bash_profile/rich_bashrc.sh" >> ~/.bashrc
+echo "source ~/bash_profile/rich_bashrc.sh" >> ~/.bashrc
 
 # Set git info
-#git config --global user.name "Richard Brown"
-#git config --global user.email "33289025+rijobro@users.noreply.github.com"
-
-# Reinstall conda -- use this if you need a more recent version of python
-#if [ "$upgrade_python" = true ] ; then
-#        conda install anaconda-clean -y
-#        anaconda-clean -y
-#        rm -rf ~/.conda ~/.jupyter /opt/conda || true
-#        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-#        bash ./Miniconda3-latest-Linux-x86_64.sh -p /opt/conda -b
-#	rm Miniconda3-latest-Linux-x86_64.sh
-
-#	# Install requirements
-#	python -m pip install -r ~/MONAI/requirements.txt
-#	python -m pip install -r ~/MONAI/requirements-dev.txt
-#fi
+git config --global user.name "Richard Brown"
+git config --global user.email "33289025+rijobro@users.noreply.github.com"
 
 # Compile MONAI cuda code
-#if [ "compilemonai" = true ]; then
-#	cd ~/MONAI
-#	BUILD_MONAI=1 python setup.py develop
-#fi
+if [ "$compilemonai" = true ]; then
+	cd ~/MONAI
+	BUILD_MONAI=1 python setup.py develop
+fi
 
 # SSH server
 if [ "$ssh_server" = true ]; then
-#	cat /rbrown/ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 	nohup /usr/sbin/sshd -D -f ~/.ssh/sshd_config -E ~/sshd.log &
 fi
 
 # Pulseaudio (send audio back to local terminal)
-#if [ "$pulseaudio" = true ]; then
-#        apt update
-#	apt install -y pulseaudio espeak
-#	echo 'export PULSE_SERVER="tcp:localhost:24713"' >> ~/.bashrc
-#	pulseaudio --start
-#fi
+if [ "$pulseaudio" = true ]; then
+        apt update
+	apt install -y pulseaudio espeak
+	echo 'export PULSE_SERVER="tcp:localhost:24713"' >> ~/.bashrc
+	pulseaudio --start
+fi
 
-#if [ "$runvnc" = true ] ; then
-#	apt update
-#	# switch commenting of next two lines for ubuntu instead of xfce4
-#	DEBIAN_FRONTEND=noninteractive apt install -y xfce4 xfce4-goodies
-#	# DEBIAN_FRONTEND=noninteractive apt install -y ubuntu-desktop
-#	apt install -y vnc4server
-#
-#	mkdir -p ~/.vnc
-#	touch ~/.vnc/xstartup
-#	chmod +x ~/.vnc/xstartup
-#
-#	# ubuntu or xfce4?
-#	echo "xfce4-session &" >> ~/.vnc/xstartup
-#	# echo "gnome-session &" >> ~/.vnc/xstartup
-#
-#	# Start the vnc server (first time requires pw)
-#	printf "$vnc_pw\n$vnc_pw\n" | vncserver
-#fi
+# Start a VNC server
+if [ "$runvnc" = true ] ; then
+	apt update
+	# switch commenting of next two lines for ubuntu instead of xfce4
+	DEBIAN_FRONTEND=noninteractive apt install -y xfce4 xfce4-goodies
+	# DEBIAN_FRONTEND=noninteractive apt install -y ubuntu-desktop
+	apt install -y vnc4server
 
-#if [ "$virtualbox" = true ] ; then
-#	#wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
-#	#wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
-#	#apt install -y software-properties-common
-#	#add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
-#	#apt update
-#	#apt install -y linux-headers-generic
-#	#apt install -y virtualbox-6.1
-#	apt install -y virtualbox
-#fi
+	mkdir -p ~/.vnc
+	touch ~/.vnc/xstartup
+	chmod +x ~/.vnc/xstartup
 
-# Use 0000 umask for shared folders (needs to happen after starting sshd service)
-#umask 0000
+	# ubuntu or xfce4?
+	echo "xfce4-session &" >> ~/.vnc/xstartup
+	# echo "gnome-session &" >> ~/.vnc/xstartup
 
-# Install python extras
-#python -m pip install -r ~/MONAI/docs/requirements.txt
-#python -m pip install ipywidgets
+	# Start the vnc server (first time requires pw)
+	printf "$vnc_pw\n$vnc_pw\n" | vncserver
+fi
 
-# Dark jupyter theme
-#python -m pip install jupyterthemes
-#jt -t oceans16 -T -N # Blue theme
-#jt -t monokai -f fira -fs 13 -nf ptsans -nfs 11 -N -kl -cursw 5 -cursc r -cellw 95% -T # Green theme
+if [ "$virtualbox" = true ] ; then
+	wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
+	apt install -y software-properties-common
+	add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+	apt update
+	apt install -y linux-headers-generic
+	apt install -y virtualbox-6.1
+	apt install -y virtualbox
+fi
 
 # Start jupyter
-#jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --notebook-dir="~" --NotebookApp.password=${jupy_pw}
+jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --notebook-dir="~" --NotebookApp.password=${jupy_pw}
 
 sleep infinity
