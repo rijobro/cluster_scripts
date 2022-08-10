8ee5e6dfbc51a5a20dbe5f12b49a8490700634e4
Richard Brown <33289025+rijobro@users.noreply.github.com>
2022-08-10 14:44:08 +0100

    add sacct

---
+++ jade/jade_sacct.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e # exit on error
+
+ndays=$1
+: ${ndays:=3}
+echo Printing jobs over last $ndays days
+SLURM_TIME_FORMAT="%d/%m %H:%M:%S"
+sacct -S now-${ndays}days -X --format="jobid,jobname%40,partition,elapsed,state,exitcode,submit,start"
+
