6fe316fb8bf4285838e3457ec9a0bbb6659d721c
Richard Brown <33289025+rijobro@users.noreply.github.com>
2022-10-28 17:03:58 +0100

    shellCheck

--- dgx/runai_startup.sh
+++ dgx/runai_startup.sh
@@ -13,23 +13,23 @@ cmd="sleep infinity"
 #####################################################################################
 print_usage()
 {
-    echo 'Script to be run at start of runai job.'
+    echo "Script to be run at start of runai job."
     echo
-    echo 'Brief syntax:'
+    echo "Brief syntax:"
     echo "${0##*/} [OPTIONS(0)...] [ : [OPTIONS(N)...]] [-- <cmd>]"
     echo
-    echo 'Full syntax:'
+    echo "Full syntax:"
     echo "${0##*/} [-h|--help] [-d|--dir <val>] [-- <cmd>]"
     echo
-    echo 'options without args:'
-    echo '-h, --help                : Print this help.'
+    echo "options without args:"
+    echo "-h, --help                : Print this help."
     echo
-    echo 'options with args:'
-    echo '-d, --dir <val>           : Directory to run from. Default: `pwd`.'
-    echo '-e, --env <name=val>      : Environmental variable, given as "NAME=VAL".'
-    echo '                            Can be used multiple times.'
+    echo "options with args:"
+    echo "-d, --dir <val>           : Directory to run from. Default: \`pwd\`."
+    echo "-e, --env <name=val>      : Environmental variable, given as \"NAME=VAL\"."
+    echo "                            Can be used multiple times."
     echo
-    echo 'NB: if `-- <cmd>` not given, `sleep infinity` is used.'
+    echo "NB: if \`-- <cmd>\` not given, \`sleep infinity\` is used."
 }
 
 #####################################################################################
@@ -55,7 +55,7 @@ while [[ $# -gt 0 ]]; do
         ;;
         -e|--env)
             if [[ -z "${envs}" ]]; then envs=(); fi
-            envs+=($1)
+            envs+=("$1")
             shift
         ;;
         *)
@@ -82,23 +82,24 @@ echo
 # Correct "~" (runai bug), source bashrc, add env vars, cd to run dir
 #####################################################################################
 
-export HOME=/nfs/home/$(whoami)
+export HOME
+HOME=/nfs/home/$(whoami)
 source /nfs/home/$(whoami)/.bashrc
 source /home/$(whoami)/.bashrc
 
 # Add any environmental variables
 for env in "${envs[@]}"; do
-    export ${env}
-    printf "export ${env}\n" >> ~/.bashrc
+    export env
+    printf "export %s\n" "${env}" >> ~/.bashrc
 done
 
-cd $run_dir
+cd "$run_dir"
 
 #####################################################################################
 # Start jupyter and sshd
 #####################################################################################
-nohup /usr/sbin/sshd -D -f /home/$(whoami)/.ssh/sshd_config -E /home/$(whoami)/.ssh/sshd.log &
-nohup jupyter notebook --ip 0.0.0.0 --no-browser --notebook-dir=".." > /home/$(whoami)/.jupyter_notebook.log 2>&1 &
+nohup /usr/sbin/sshd -D -f "/home/$(whoami)/.ssh/sshd_config" -E "/home/$(whoami)/.ssh/sshd.log" &
+nohup jupyter notebook --ip 0.0.0.0 --no-browser --notebook-dir=".." > "/home/$(whoami)/.jupyter_notebook.log" 2>&1 &
 
 #####################################################################################
 # Execute command
@@ -107,6 +108,6 @@ nohup jupyter notebook --ip 0.0.0.0 --no-browser --notebook-dir=".." > /home/$(w
 # thus allowing python code to catch the signal and execute a callback
 trap 'trap " " SIGTERM; kill 0; wait' SIGTERM
 
-echo running ${cmd}
+echo running "${cmd}"
 ${cmd} &
 wait $!
